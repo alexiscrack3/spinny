d6db6490c40ceb1cf2d0ffbde4301a527c5e310d
Alexis Ortega <alexis.oc3@gmail.com>
2021-03-26 23:08:21 -0600

    Validate club name in tests

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,2 +1,3 @@
 class Club < ApplicationRecord
+  validates :name, presence: true
 end
--- test/models/club_test.rb
+++ test/models/club_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class ClubTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "club should be valid" do
+    club = Club.new(name: 'name')
+    assert club.valid?
+  end
+
+  test 'club should be invalid without name' do
+    club = Club.new(name: '')
+    refute club.valid?, 'club is valid without a name'
+    assert_not_nil club.errors[:name], 'no validation error for name present'
+  end
 end
