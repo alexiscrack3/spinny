3ca8717f29d82e764a3df60c024530841ee5892d
Alexis Ortega <alexis.oc3@gmail.com>
2021-03-26 23:30:19 -0600

    Restrict routes for clubs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :clubs
+  resources :clubs, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'clubs#index'
   get 'static_pages/home'
