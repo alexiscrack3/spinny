d3ae0e9cbb5a256234f0a5ba3020160d52ef0312
Alexis Ortega <alexis.oc3@gmail.com>
2021-03-27 09:47:42 -0600

    Add players to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,17 @@ require 'faker'
 #   Character.create(name: 'Luke', movie: movies.first)
 
 (1..10).each do |index|
-  club = Club.create(name: Faker::FunnyName.name, description: Faker::Lorem.sentence(word_count: 1 + rand(8)))
+  player = Player.new(
+    first_name: Faker::Name.unique.first_name,
+    last_name: Faker::Name.unique.last_name,
+    email: Faker::Internet.email
+  )
+  player.save
+end
+
+(1..10).each do |index|
+  club = Club.create(
+    name: Faker::FunnyName.name,
+    description: Faker::Lorem.sentence(word_count: 1 + rand(8))
+  )
 end
