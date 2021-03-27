063fc38b0c0829adba8f37c890d765b0e199b80e
Alexis Ortega <alexis.oc3@gmail.com>
2021-03-26 23:49:35 -0600

    Generate Player resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :players
   resources :clubs, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'clubs#index'
