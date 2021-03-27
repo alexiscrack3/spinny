aeb69d4d6831e71f883bbb59e1faef44bbb0610d
Alexis Ortega <alexis.oc3@gmail.com>
2021-03-26 21:25:37 -0600

    Generate Club resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :clubs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
   get 'static_pages/home'
